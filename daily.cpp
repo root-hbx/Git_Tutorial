23e5ede535aeb1d63bf8edf07b19d980fab272e6
root-hbx <huboxuan2004@gmail.com>
2024-02-09 02:01:55 +0800

    first

---
+++ daily.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: daily.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2024年02月09日 星期五 01时55分15秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+	cin >> a;
+    cout<<a<<endl;
+    return 0;
+}
