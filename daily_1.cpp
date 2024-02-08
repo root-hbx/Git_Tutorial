ea892d4b39a84ec7635e7213f952921788a82a9f
root-hbx <huboxuan2004@gmail.com>
2024-02-09 02:28:08 +0800

    commit 1

---
+++ daily_1.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: daily_1.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2024年02月09日 星期五 02时09分00秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hbx nb!"<<endl;
+    return 0;
+}
