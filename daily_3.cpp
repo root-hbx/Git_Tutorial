9fdf6a073dfc395c0365b1ff4d3877633918c670
root-hbx <huboxuan2004@gmail.com>
2024-02-09 02:28:52 +0800

    commit 3

---
+++ daily_3.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: daily_3.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2024年02月09日 星期五 02时24分20秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int hbx;
+	cin >> hbx;
+	cout<<hbx<<endl;
+    return 0;
+}
