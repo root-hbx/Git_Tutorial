32046fb07e09f7bba52781ee5145eb40d962e83a
root-hbx <huboxuan2004@gmail.com>
2024-02-09 11:05:06 +0800

    branch test 1

---
+++ test_branch.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test_branch.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2024年02月09日 星期五 11时02分23秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"I belong to branch master"<<endl;
+	return 0;
+}
