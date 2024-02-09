2d4bcaacbb4e659132dffaa0fe4d38a4f405b0f8
root-hbx <huboxuan2004@gmail.com>
2024-02-09 11:39:42 +0800

    branch test 2

---
+++ test_branch1.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test_branch1.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2024年02月09日 星期五 11时38分32秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello hbx"<<endl;
+    cout<<2024<<endl;
+    return 0;
+}
